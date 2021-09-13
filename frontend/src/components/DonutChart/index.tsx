0e6bf9d81885fec9922f11bb24bd23d31a53a70d
Debora Ferreira <deborayferreirah@gmail.com>
2021-09-12 22:01:11 -0300

    first commit

---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import { SalesSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+}
+
+const DonutChart = () => {
+
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-saller`)
+      .then(response => {
+        const data = response.data as SalesSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        setChartData({ labels: myLabels, series: mySeries });
+        console.log(chartData)
+      });
+  })
+
+
+
+  /*
+  Gera duplicitade em chamadas
+  FORMA ERRADA
+  let chartData: ChartData = { labels: [], series: [] };
+
+   FORMA ERRADA
+  axios.get(`${BASE_URL}/sales/amount-by-saller`)
+    .then(response => {
+      const data = response.data as SalesSum[];
+      const myLabels = data.map(x => x.sellerName);
+      const mySeries = data.map(x => x.sum);
+
+      chartData = { labels: myLabels, series: mySeries }
+      console.log(chartData)
+    });
+
+  const mockData = {
+     series: [477138, 499928, 444867, 220426, 473088],
+    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  }
+  */
+
+  const options = {
+    legend: {
+      show: true
+    }
+  }
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+
+    />
+  );
+}
+
+export default DonutChart;
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,35 @@
+import { Seller } from "./saller"
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  size?: number;
+  number: number;
+  first: boolean;
+  numberOfElements?: number;
+  empty?: boolean;
+
+
+}
+
+export type SalesSum = {
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess = {
+  sellerName: string;
+  visited: number;
+  deals: number;
+}
